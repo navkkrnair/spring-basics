f3123a01c6047b3780f84700e96ce658f4679a30
Krishnakumar R <krishna.ramachandran1973@gmail.com>
2024-02-26 12:26:45 +0530

    spring core complete

--- src/main/java/com/wipro/app/profiles/Main.java
+++ src/main/java/com/wipro/app/profiles/Main.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MutablePropertySources;
 
 import com.wipro.app.profiles.model.Food;
 import com.wipro.app.profiles.service.FoodProviderService;
@@ -11,12 +13,16 @@ import com.wipro.app.profiles.service.FoodProviderService;
 public class Main {
 	public static void main(String[] args) {
 		GenericApplicationContext ctx = new AnnotationConfigApplicationContext(ProfilesConfigurer.class);
+		ConfigurableEnvironment env = ctx.getEnvironment();
+		env.setActiveProfiles("kindergarten");
+		MutablePropertySources propertySources = env.getPropertySources();
 
 		FoodProviderService foodProviderService = ctx.getBean(FoodProviderService.class);
 		List<Food> lunchSet = foodProviderService.provideLunchSet();
 		for (Food food : lunchSet) {
 			System.out.println("Food: " + food.getName());
 		}
+
 		ctx.close();
 
 	}
